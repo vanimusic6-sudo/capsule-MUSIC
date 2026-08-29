2e88c13deaee1eb9a36ce8a73c43f70836e4aa2c
vani-s <vanimusic6@gmail.com>
2026-08-30 01:15:48 +0700

    Add 'Sources' settings item to VeluneSettingsScreen

--- app/src/main/kotlin/com/nikhil/yt/ui/screens/settings/VeluneSettingsScreen.kt
+++ app/src/main/kotlin/com/nikhil/yt/ui/screens/settings/VeluneSettingsScreen.kt
@@ -1,5 +1,4 @@
 
-/* * capsule fork
  * Based on Velune by Nikhil
  * Licensed Under GPL-3.0
  */
@@ -142,6 +141,14 @@ fun VeluneSettingsScreen(
                     icon = painterResource(R.drawable.multi_user),
                     title = "Listen Together",
                     onClick = { navController.navigate("settings/music_together") }
+                )
+            }
+
+            item {
+                SettingsItemScreenshotStyle(
+                    icon = painterResource(R.drawable.play),
+                    title = "Sources",
+                    onClick = { navController.navigate("settings/sources") }
                 )
             }
 
