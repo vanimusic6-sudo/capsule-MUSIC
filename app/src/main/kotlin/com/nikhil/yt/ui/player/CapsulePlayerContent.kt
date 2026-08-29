752b6330937a8689a7e97f80c38890e2bfeeedfb
vani-s <vanimusic6@gmail.com>
2026-08-29 15:47:35 +0700

    Update CapsulePlayerContent.kt

--- app/src/main/kotlin/com/nikhil/yt/ui/player/CapsulePlayerContent.kt
+++ app/src/main/kotlin/com/nikhil/yt/ui/player/CapsulePlayerContent.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -68,7 +69,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.input.pointer.awaitFirstDown
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
