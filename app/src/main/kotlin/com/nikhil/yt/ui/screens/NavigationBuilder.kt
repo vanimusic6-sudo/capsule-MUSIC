ff882415a7b8e108240d7885f921725443a0f76c
vani-s <vanimusic6@gmail.com>
2026-08-30 11:28:42 +0700

    Update NavigationBuilder.kt

--- app/src/main/kotlin/com/nikhil/yt/ui/screens/NavigationBuilder.kt
+++ app/src/main/kotlin/com/nikhil/yt/ui/screens/NavigationBuilder.kt
@@ -1,5 +1,5 @@
 
-/** Velune - by Nikhil
+ /** Velune - by Nikhil
  * Nikhil
  * Licensed Under GPL-3.0
  */
@@ -84,7 +84,6 @@ import com.nikhil.yt.ui.screens.settings.PoTokenScreen
 import com.nikhil.yt.ui.screens.settings.PrivacySettings
 import com.nikhil.yt.ui.screens.settings.SettingsScreen
 import com.nikhil.yt.ui.screens.settings.StorageSettings
-import com.nikhil.yt.ui.screens.settings.SourcesSettings
 import com.nikhil.yt.ui.screens.settings.ThemeCreatorScreen
 import com.nikhil.yt.ui.utils.ShowMediaInfo
 import com.nikhil.yt.utils.rememberEnumPreference
@@ -320,9 +319,6 @@ fun NavGraphBuilder.navigationBuilder(
     }
     composable("settings/player") {
         PlayerSettings(navController, scrollBehavior)
-    }
-    composable("settings/sources") {
-        SourcesSettings(navController)
     }
     composable("settings/storage") {
         StorageSettings(navController, scrollBehavior)
