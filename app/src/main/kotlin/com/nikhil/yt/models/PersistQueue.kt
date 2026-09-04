f2b94b5832fa80f3586fdeb7af9985669954ca12
vani-s <vanimusic6@gmail.com>
2026-09-04 19:31:14 +0700

    Stabilize persisted queue hierarchy serialization

--- app/src/main/kotlin/com/nikhil/yt/models/PersistQueue.kt
+++ app/src/main/kotlin/com/nikhil/yt/models/PersistQueue.kt
@@ -24,21 +24,36 @@ data class PersistQueue(
 }
 
 sealed class QueueType : Serializable {
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+
     object LIST : QueueType() {
         private const val serialVersionUID = 1L
+        private fun readResolve(): Any = LIST
     }
+
     object YOUTUBE : QueueType() {
         private const val serialVersionUID = 1L
+        private fun readResolve(): Any = YOUTUBE
     }
+
     object YOUTUBE_ALBUM_RADIO : QueueType() {
         private const val serialVersionUID = 1L
+        private fun readResolve(): Any = YOUTUBE_ALBUM_RADIO
     }
+
     object LOCAL_ALBUM_RADIO : QueueType() {
         private const val serialVersionUID = 1L
+        private fun readResolve(): Any = LOCAL_ALBUM_RADIO
     }
 }
 
 sealed class QueueData : Serializable {
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+
     data class YouTubeData(
         val endpoint: String,
         val continuation: String? = null
