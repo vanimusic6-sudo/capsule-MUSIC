b2d68202de6305584682d761499a22f37ba4e0c2
vani-s <vanimusic6@gmail.com>
2026-09-26 14:32:47 +0700

    Import width modifier for Light mode atoms

--- app/src/main/kotlin/com/nikhil/yt/ui/player/CapsulePlaybackModeToggle.kt
+++ app/src/main/kotlin/com/nikhil/yt/ui/player/CapsulePlaybackModeToggle.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
